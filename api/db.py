66b61111dac5c58c16fd04157d308e7f39fb92ae
SouMurate04 <somrt2004@gmail.com>
2026-05-27 20:55:19 +0900

    Add db.py

---
+++ api/db.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "ca": "certs/server-ca.pem",
+            "cert": "certs/client-cert.pem",
+            "key": "certs/client-key.pem",
+        }
+    }
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
