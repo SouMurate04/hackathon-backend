cac673f8d22aa292efe27e883cf1becdb1c67fda
SouMurate04 <somrt2004@gmail.com>
2026-06-10 00:43:40 +0900

    fix browse.py

--- api/cruds/browse.py
+++ api/cruds/browse.py
@@ -9,24 +9,26 @@ import api.models as model
 import api.schemas.item as item_schema
 
 async def get_items(db: AsyncSession) -> List[item_schema.ListedItem]:
-    query = select(
-        model.Item.id.label["id"],
-        model.Item.name.label["name"],
-        model.Item.description.label["description"],
-        model.Item.price.label["price"],
-        model.Item.posted_at.label["posted_at"],
-        model.Category.name.label["category"],
-        model.User.name.label["seller"],
-        model.Image.url.label["image_url"],
-        model.Tag.name.label["tag"],
+    (query = select(
+        model.Item.id.label("id"),
+        model.Item.name.label("name"),
+        model.Item.description.label("description"),
+        model.Item.price.label("price"),
+        model.Item.posted_at.label("posted_at"),
+        model.Category.name.label("category"),
+        model.User.name.label("seller"),
+        model.Image.url.label("image_url"),
+        model.Tag.name.label("tag"),
     ).join(model.User, model.Item.seller_id == model.User.id)
         .outerjoin(model.Category, model.Item.category_id == model.Category.id)
         .outerjoin(model.Image, model.Item.id == model.Image.item_id)
         .outerjoin(model.Tag, model.Item.id == model.Tag.item_id)
-        .order_by(model.Item.posted_at.desc())
+        .order_by(model.Item.posted_at.desc()))
 
     result = await db.execute(query)
     rows = result.mappings().all()
+
+    items_by_id = {}
 
     for row in rows:
         item_id = row["id"]
