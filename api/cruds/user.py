bdf52e8040389deee8768173322733fd033b2d48
SouMurate04 <somrt2004@gmail.com>
2026-06-10 22:27:49 +0900

    Add UserPage

--- api/cruds/user.py
+++ api/cruds/user.py
@@ -15,11 +15,18 @@ async def create_user(db: AsyncSession, new_user: user_schema.NewUser, firebase_
     await db.refresh(record)
     return
 
-async def get_user(db: AsyncSession, firebase_uid: str) -> user_schema.User:
+async def get_user_me(db: AsyncSession, firebase_uid: str) -> user_schema.User:
     result = await db.execute(
         select(model.User).where(model.User.firebase_uid == firebase_uid)
     )
     return result.scalars().first() 
+
+async def get_user(db: AsyncSession, user_id: int) -> user_schema.User:
+    result = await db.execute(
+        select(model.User).where(model.User.id == user_id)
+    )
+    return result.scalars().first() 
+
 
 async def update_user(db: AsyncSession, firebase_uid: str, request: user_schema.UpdatedUser):
     user = await get_user(db, firebase_uid)
--- api/routers/user.py
+++ api/routers/user.py
@@ -19,12 +19,16 @@ async def create_user(request: user_schema.NewUser, db: AsyncSession = Depends(g
     return await user_crud.create_user(db, request, firebase_uid)
 
 # ユーザー情報を取得
-@router.get("/user", response_model=user_schema.User)
-async def get_user(uid: str, db: AsyncSession = Depends(get_db),
+@router.get("/user/me", response_model=user_schema.User)
+async def get_user_me(db: AsyncSession = Depends(get_db),
     firebase_user: dict = Depends(get_current_firebase_user)):
 
     firebase_uid = firebase_user["uid"]
-    return await user_crud.get_user(db, uid)
+    return await user_crud.get_user(db, firebase_uid)
+
+@router.get("/user/{user_id}", response_model=user_schema.User)
+async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
+    return await user_crud.get_user(db, user_id)
 
 # ユーザー情報を更新
 @router.put("/user", response_model=None)
